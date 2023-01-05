a92af795a3bcc9ac21008e86a34d2cf14ada18f5
Eric Slutz <eric.slutz@icloud.com>
2023-01-05 15:13:18 -0500

    completed day 1 part 2

--- AdventOfCode2022/Day1.cs
+++ AdventOfCode2022/Day1.cs
@@ -4,6 +4,7 @@
     {
         private readonly string _inputFile = "Input/Day_1_Input.txt";
         private readonly Elf _elfWithMostCalories;
+        private readonly Elf[] _topElfsWithMostCalories;
         private readonly List<Elf> _elfs = new();
 
         public Day1()
@@ -30,6 +31,16 @@
 
             _elfWithMostCalories = Part1_GetElfWithMostCalories();
             Console.WriteLine($"Elf {_elfWithMostCalories.ElfNumber} has the most calories of {_elfWithMostCalories.Calories}");
+
+            _topElfsWithMostCalories = Part2_GetTopThreeElfs();
+            Console.WriteLine("\nThe top three elves with the most calories are:");
+            foreach(var elf in _topElfsWithMostCalories)
+            {
+                Console.WriteLine($"Elf {elf.ElfNumber} with {elf.Calories} calories");
+            }
+
+            var topElvesCaloriesCombined = _topElfsWithMostCalories.Select(elf => elf.Calories).Sum();
+            Console.WriteLine($"\nThe top three elves have a combined caloric content of: {topElvesCaloriesCombined} calories");
         }
 
         private Elf Part1_GetElfWithMostCalories()
@@ -43,6 +54,11 @@
                 }
             }
             return mostCaloricElf;
+        }
+
+        private Elf[] Part2_GetTopThreeElfs()
+        {
+            return _elfs.OrderByDescending(elf => elf.Calories).Take(3).ToArray();
         }
     }
 
